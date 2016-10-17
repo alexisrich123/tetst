7e4866df9ccddfeed89759ed9956f0e43c996079
SK207_24 <SK207_24@K207_24.carrollcc.edu>
2016-10-17 18:19:22 -0400

    why

---
+++ src/sasfas/sdas.java
@@ -0,0 +1,16 @@
+package sasfas;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class sdas {
+
+	public static void main(String[] args) throws FileNotFoundException {
+		int an;
+		Scanner key= new Scanner(System.in);
+		System.out.println("enter an number between 1-100");
+		FileInputStream file=new FileInputStream("file.dat");
+	}
+
+}
